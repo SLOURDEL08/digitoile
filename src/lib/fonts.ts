278213e5a6b1bee9622beefb5cd79e344a878596
SebLOURDEL <seb.lourdel8@gmail.com>
2024-11-17 19:41:27 +0100

    font path

--- src/lib/fonts.ts
+++ src/lib/fonts.ts
@@ -4,32 +4,32 @@ import localFont from 'next/font/local';
 export const customFont = localFont({
   src: [
     {
-      path: '../../public/fonts/ClashDisplay-ExtraLight.otf',
+      path: '/fonts/ClashDisplay-ExtraLight.otf',
       weight: '200',
       style: 'normal',
     },
     {
-      path: '../../public/fonts/ClashDisplay-Light.otf',
+      path: '/fonts/ClashDisplay-Light.otf',
       weight: '300',
       style: 'normal',
     },
     {
-      path: '../../public/fonts/ClashDisplay-Regular.otf',
+      path: '/fonts/ClashDisplay-Regular.otf',
       weight: '400',
       style: 'normal',
     },
     {
-      path: '../../public/fonts/ClashDisplay-Medium.otf',
+      path: '/fonts/ClashDisplay-Medium.otf',
       weight: '500',
       style: 'normal',
     },
     {
-      path: '../../public/fonts/ClashDisplay-SemiBold.otf',
+      path: '/fonts/ClashDisplay-SemiBold.otf',
       weight: '600',
       style: 'normal',
     },
     {
-      path: '../../public/fonts/ClashDisplay-Bold.otf',
+      path: '/fonts/ClashDisplay-Bold.otf',
       weight: '700',
       style: 'normal',
     },
