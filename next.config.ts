a932dfe4115ce65faed1c7cc5d94427b8f15eac4
SebLOURDEL <seb.lourdel8@gmail.com>
2024-11-17 20:30:38 +0100

    rebuild

--- next.config.ts
+++ next.config.ts
@@ -1,9 +1,17 @@
 import type { NextConfig } from "next";
 
 const nextConfig: NextConfig = {
-  /* config options here */
-    reactStrictMode: true,
-
+  reactStrictMode: true,
+  webpack: (config) => {
+    config.module.rules.push({
+      test: /\.(woff|woff2|eot|ttf|otf)$/i,
+      type: 'asset/resource',
+      generator: {
+        filename: 'static/fonts/[name][ext]',
+      },
+    });
+    return config;
+  },
 };
 
 export default nextConfig;
