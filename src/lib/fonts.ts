5dbf96aadda722d0d16d7afd84bc19d608fad203
SebLOURDEL <seb.lourdel8@gmail.com>
2024-11-17 20:02:19 +0100

    fonts src

--- src/lib/fonts.ts
+++ src/lib/fonts.ts
@@ -3,36 +3,36 @@ import localFont from 'next/font/local';
 export const customFont = localFont({
   src: [
     {
-      path: '/fonts/ClashDisplay-ExtraLight.otf',  // Correction ici
+      path: './fonts/ClashDisplay-ExtraLight.otf',
       weight: '200',
       style: 'normal',
     },
     {
-      path: '/fonts/ClashDisplay-Light.otf',  // Correction ici
+      path: './fonts/ClashDisplay-Light.otf',
       weight: '300',
       style: 'normal',
     },
     {
-      path: '/fonts/ClashDisplay-Regular.otf',  // Correction ici
+      path: './fonts/ClashDisplay-Regular.otf',
       weight: '400',
       style: 'normal',
     },
     {
-      path: '/fonts/ClashDisplay-Medium.otf',  // Correction ici
+      path: './fonts/ClashDisplay-Medium.otf',
       weight: '500',
       style: 'normal',
     },
     {
-      path: '/fonts/ClashDisplay-SemiBold.otf',  // Correction ici
+      path: './fonts/ClashDisplay-SemiBold.otf',
       weight: '600',
       style: 'normal',
     },
     {
-      path: '/fonts/ClashDisplay-Bold.otf',  // Correction ici
+      path: './fonts/ClashDisplay-Bold.otf',
       weight: '700',
       style: 'normal',
     },
   ],
-  variable: '--clashdisplay', // Pour utiliser en tant que variable CSS
+  variable: '--clashdisplay',
   display: 'swap',
 });
